d862d4082386f844074aa1df4d6a27f231ec4a35
IsaiasJD <ijdec68@gmail.com>
2019-04-29 21:15:57 -0700

    update to css

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
-import React, { Component } from 'react';
-import Contestants from './components/Contestants';
-import './App.css';
+import React, { Component } from "react";
+import Contestants from "./components/Contestants";
+import Header from "./components/layout/Header";
+import "./App.css";
 
 class App extends Component {
   state = {
     contestants: [
       {
         id: 1,
-        firstName: 'Shannon', 
-        lastName: 'Curtis', 
-        startingWeight: 250, 
+        firstName: "Shannon",
+        lastName: "Curtis",
+        startingWeight: 250,
         currentWeight: 245
       },
       {
         id: 2,
-        firstName: 'Deisy', 
-        lastName: 'Saavedra', 
-        startingWeight: 220, 
+        firstName: "Deisy",
+        lastName: "Saavedra",
+        startingWeight: 220,
         currentWeight: 221
       },
       {
         id: 3,
-        firstName: 'Alva', 
-        lastName: 'Perez', 
-        startingWeight: 120, 
+        firstName: "Alva",
+        lastName: "Perez",
+        startingWeight: 120,
         currentWeight: 117
       }
     ]
+  };
+  componentDidMount() {
+    const contestants = [].concat(this.state.contestants).map(c => {
+      const percentLost =
+        Math.round(
+          ((c.startingWeight - c.currentWeight) / c.startingWeight) * 10000
+        ) / 100;
+      return { ...c, percentLost };
+    });
+    this.setState({ contestants });
   }
-  render() {
-  return (
-    <div className="App">
-      <h1>The Biggest Loser</h1>
-      <Contestants contestants={this.state.contestants} />
 
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <div className="background">
+          <Contestants contestants={this.state.contestants} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Contestants.js
+++ src/components/Contestants.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
-import Conversion from './Conversion'
+import React, { Component } from "react";
+import Background from './images/background.jpg';
 
-class Contestants extends Component {
-    render() {
-        console.log(this.props.contestants);
-        return this.props.contestants.map((contestant) => (
-            <div>
-            <h3>{contestant.firstName} {contestant.lastName}</h3>
-            <Conversion contestant={contestant}/>
-            </div>
-        ));
-    }
+export default function Contestants(props) {
+  return props.contestants
+    .sort((a, b) => b.percentLost - a.percentLost)
+    .map((contestant, i) => (
+        <div key={i} style={ contestantsrStyle.div }>
+            <h3 style={ contestantsrStyle.h3 }>
+                 {i+1}. {contestant.firstName} {contestant.lastName}
+            </h3>
+            <p style={ contestantsrStyle.p }>Percentage of Weight Loss:</p>
+            <p style={ contestantsrStyle.p }>{contestant.percentLost}</p>
+        </div>
+    ));
 }
 
-export default Contestants;
+const contestantsrStyle = {
+    div:{
+    color: '#000000', 
+    textAlign: 'center', 
+    padding: '10px',
+    margin: '15px 0px 15px 0px',
+    },
+    h3: {
+        padding: '1px',
+    },
+    p: {
+        padding: '1px',
+    }
+}
